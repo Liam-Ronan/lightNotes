7ed80b0e91f87de93434945fbda525d0cbab78e7
Liam Ronan <liamronan16@gmail.com>
2022-10-09 17:48:40 +0100

    began working on notes project, need to add views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/notes', [NoteController::class, 'index']);
+Route::get('/notes/create', [NoteController::class, 'create']);
+Route::get('/notes', [NoteController::class, 'store']);
+
+Route::resource('/notes', NoteController::class);
+
+Route::resource('/notes', NoteController::class)->middleware(['auth']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
